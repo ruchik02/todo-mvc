7a52740fb275cfa329de18303464e36dccfc8bc3
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2023-01-16 16:44:02 +0530

    error and item not show in frontend

--- public/index.js
+++ public/index.js
@@ -1,95 +1,262 @@
-// let todos = [];
-// let res = false;
-// function gettodos(){
-//     fetch("/").then((response) => {
-//         return response.text();
-//     }).then((data) => {
-//         console.log(data);
-//         // todos = JSON.parse(data);
-//         console.log(todos,"8");
-//         obj.show();
-//     })
-// }
-// class todo {
-//     constructor(text, isCompleted) {
-//         this.text = text;
-//         this.isCompleted = isCompleted;
-//     }
-// }
-// class myTodo{
-//     constructor(){
-//     }
-//     add(task) {
-//         console.log(task,"23");
-//         fetch('/addtodos', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(task),
-//         })
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log('Success:', data);
-//                 todos = data;
-//                 obj.show();
-//                 console.log(todos,"35");
-//             })
-//             .catch((error) => {
-//                 console.error('Error:', error);
-//             });
-//     }
-//     show(){
-//         let todos1=document.getElementById("todos");
-//         console.log(todos,"120");
-//         todos1.innerHTML="";
-//         let all=document.getElementById('all');
-//         all.style.border=`1px solid rgba(175, 47, 47, 0.2)`
-//         active.style.border=`none`
-//         completed.style.border=`none`
-//         let html=``;
-//         for(let todo1 in this.todos){
-//             if(this.todos[todo1].isCompleted===true){
-//                 html+=`<div class="todos" id="todos">
-//                 <ul id="ul" class="todo-list">
-//                     <li>
-//                     <input type="checkbox" onclick="obj.toggle(event,${todo1})" checked name="checkbox" id="checker" class="check-box">
-//                     <label for="todoLbael" class="data line-through">${this.todos[todo1].text}</label>
-//                     <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
-//                 </li>
-//                 </ul>
-//             </div>`
+let todos = [];
+let res = false;
+function getTodos() {
+    fetch("/gettodos").then((response) => {
+        return response.text();
+    }).then((data) => {
+        todos = JSON.parse(data);
+        console.log(todos,"8");
+        obj.show();
+        obj.count();
+    })
+}
+class todo {
+    constructor(text, completed) {
+        this.text = text;
+        this.completed = completed;
+    }
+}
+class myTodo {
+    constructor() {}
+    // count
+    count() {
+        let count = 0;
+        for (let i in todos) {
+            if (todos[i].completed == false) {
+                count++;
+            }
+        }
+        let countValue = document.getElementById("count");
+        countValue.innerText = count;
+        console.log(countValue,count,"31");
+    }
+    // clearCompleted
+    clearCompleted() {
+        fetch("/clearcompleted").then((response) => {
+            return response.text();
+        }).then((data) => {
+            todos = JSON.parse(data);
+            console.log(todos);
+            obj.show();
+            obj.count();
+        })
+    }
 
-//             }
-//             else{
-//                 html+=`<div class="todos" id="todos">
-//                 <ul id="ul" class="todo-list">
-//                     <li>
-//                     <input type="checkbox" onclick="obj.toggle(event,${todo1})" name="checkbox" id="checker" class="check-box">
-//                     <label for="todoLbael" class="data">${this.todos[todo1].text}</label>
-//                     <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
-//                 </li>
-//                 </ul>
-//             </div>`
-//             }
-//         }
-//         todos1.innerHTML=html;
+    // toggle 
+    toggle1() {
+        if (res == false) {
+            fetch("/markallcompleted").then((response) => {
+                return response.text();
+            }).then((data) => {
+                todos = JSON.parse(data);
+                console.log(todos);
+                obj.show();
+                obj.count();
+            })
+            res = true;
+        }
+        else {
+            fetch("/markalluncompleted").then((response) => {
+                return response.text();
+            }).then((data) => {
+                todos = JSON.parse(data);
+                console.log(todos);
+                obj.show();
+                obj.count();
+            })
+            res = false;
+        }
+        this.show();
+        this.count();
+    }
 
-//     }
-// }
-// let inputText=document.getElementById('input-text');
-//     let obj=new myTodo();
-//     gettodos();
-//     inputText.addEventListener("keypress",function(e){
-//         if(e.key==='Enter' && inputText.value!=""){
-//             let footer=document.getElementById('footer');
-//             footer.style.visibility="visible";
-//             let text=e.target.value;
-//             let task=new todo(text,false);
-//             obj.add(task);
-//             inputText.value=null;
-//             console.log(obj,"89");
-//             obj.show();
-//             // obj.count();
-//         }
-//     })
+    // remove element
+    remove(i) {
+        let id = todos[i]._id;   
+        fetch('/removetodo/' + id, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                todos = data;
+                obj.show();
+                obj.count();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+    // toggle 
+    toggle(e, i) {
+        if (e.target.checked) {
+            let id = todos[i]._id;
+            fetch('/markcomplete/' + id, {
+                method: 'PATCH'
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    todos = data;
+                    obj.show();
+                    obj.count();
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+        else {
+            let id = todos[i]._id;
+            fetch('/markuncomplete/' + id, {
+                method: 'PATCH'
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    todos = data;
+                    obj.show();
+                    obj.count();
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    }
+
+    // add task
+    add(task) {
+        console.log(task);
+        fetch('/addtodo', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(task),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                todos = data;
+                obj.show();
+                obj.count();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    // showCompleted
+    showCompleted() {
+        console.log("showCompleted")
+        let todo = document.getElementById("todos");
+            todo.innerHTML = "";
+            active.style.border=`none`
+            all.style.border='none'
+            let completed=document.getElementById('completed');
+            completed.style.border=`1px solid rgba(175, 47, 47, 0.2)`
+        let html = ``;
+        for (let todo1 in todos) {
+            if (todos[todo1].completed === true) {
+                html += `<div class="todos" id="todos">
+                <ul id="ul" class="todo-list">
+                    <li>
+                    <input type="checkbox" onclick="obj.toggle(event,${todo1})" checked name="checkbox" id="checker" class="check-box">
+                    <label for="todoLbael" class="data line-through">${todos[todo1].text}</label>
+                    <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
+                </li>
+                </ul>
+            </div>`;
+            }
+        }
+        todo.innerHTML = html;
+    }
+    // showActive
+    showActive() {
+        let todo = document.getElementById("todos");
+        todo.innerHTML = "";
+        all.style.border='none'
+        completed.style.border=`none`
+        let active=document.getElementById('active');
+        active.style.border=`1px solid rgba(175, 47, 47, 0.2)`
+        let html = ``;
+        for (let todo1 in todos) {
+            if (todos[todo1].completed === false) {
+                html += `<div class="todos" id="todos">
+                <ul id="ul" class="todo-list">
+                    <li>
+                    <input type="checkbox" onclick="obj.toggle(event,${todo1})" name="checkbox" id="checker" class="check-box">
+                    <label for="todoLbael" class="data">${todos[todo1].text}</label>
+                    <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
+                </li>
+                </ul>
+            </div>`
+            }
+        }
+        todo.innerHTML = html;
+    }
+    // show
+    show() {
+        let todo = document.getElementById("todos");
+        todo.innerHTML = "";
+        let all = document.getElementById("all");
+        all.style.border = `1px solid rgba(175, 47, 47, 0.2)`;
+        active.style.border = `none`;
+        completed.style.border = `none`;
+        let html = ``;
+        for (let todo1 in todos) {
+          if (todos[todo1].completed === true) {
+            html += `<div class="todos" id="todos">
+                    <ul id="ul" class="todo-list">
+                        <li>
+                        <input type="checkbox" onclick="obj.toggle(event,${todo1})" checked name="checkbox" id="checker" class="check-box">
+                        <label for="todoLbael" class="data line-through">${todos[todo1].text}</label>
+                        <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
+                    </li>
+                    </ul>
+                </div>`;
+          } else {
+            html += `<div class="todos" id="todos">
+                    <ul id="ul" class="todo-list">
+                        <li>
+                        <input type="checkbox" onclick="obj.toggle(event,${todo1})" name="checkbox" id="checker" class="check-box">
+                        <label for="todoLbael" class="data">${todos[todo1].text}</label>
+                        <label for="todoCross" class="cross" onclick=(obj.remove(${todo1}))>X</label>
+                    </li>
+                    </ul>
+                </div>`;
+          }    
+        }
+        todo.innerHTML = html;
+    }
+}
+// enter the key value
+let inputText = document.getElementById("input-text");
+let obj = new myTodo();
+getTodos();
+inputText.addEventListener("keypress", function (e) {
+    if (e.key == 'Enter' && inputText.value != "") {
+        let text = e.target.value;
+        let task = new todo(text, false);
+        obj.add(task);
+        inputText.value = null;
+    }
+})
+// showActive
+function showActive() {
+    obj.showActive();
+}
+// showAll
+function showAll() {
+    obj.show();
+}
+// showCompleted
+function showCompleted(){
+    obj.showCompleted()
+}
+// clearCompleted
+function clearCompleted() {
+    obj.clearCompleted();
+}
+function completedAll() {
+    obj.toggle1();
+}
--- src/app.js
+++ src/app.js
@@ -1,64 +1,48 @@
-const express=require('express');
-const app=express();
-const port=8000;
-const path=require('path');
-const fs=require('fs');
-const staticPath=path.join(__dirname,"../public");
+const express = require("express");
+const app = express();
+const port = 8000;
+const path = require("path");
+const fs = require("fs");
+const staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.get('/', async (req, res) => {
-    try {
-        let response = await fetch('db.json');
-        let todos = await response.json();
-        res.status(200).json(todos);
-        fs.readFile('db.json',(err,data)=>{
-            if(err) throw err;
-            let jsonData=JSON.parse(data);
-          })
-        console.log(todos,"14");
-    } catch (err) {
-        console.error(err);
-        let data = await fs.promises.readFile('db.json', 'utf8');
-        let todos = JSON.parse(data);
-        res.status(200).json(todos);
-        console.log(todos,"20");
-    }
-});
-app.post('/addtodos', async (req, res) => {
-    try {
-        let result=req.body.todo;
-        const text=req.body.text;
-        const isCompleted = req.body.isCompleted;
-        let response = await fetch('db.json');
-        let todos = await response.json();
-        const todo = {
-            task: result,
-            isCompleted: false,
-            id:Date.now()
-          };
-        // todos.push(todo);
-        await fetch('db.json', {
-            method: 'PUT',
-            body: JSON.stringify(todos),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        // res.status(201).send("To-do item added successfully");
-        res.redirect('/');
-    } catch (err) {
-        console.error(err);
-        let data = await
-         fs.promises.readFile('db.json', 'utf8');
-        let todos = JSON.parse(data);
-        // todos.push(todo);
-        await fs.promises.writeFile('db.json', JSON.stringify(todos));
-        res.status(201).send("To-do item added successfully");
-    }
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/gettodos", async (req, res) => {
+  try {
+    let response = await fs.promises.readFile("src/db.json", "utf8");
+    let todos = await JSON.parse(response);
+    res.status(200).json(todos);
+    fs.readFile("src/db.json", (err, data) => {
+      if (err) throw err;
+      let jsonData = JSON.parse(data);
+    });
+    console.log(todos, "14");
+  } catch (err) {
+    console.error(err);
+  }
 });
 
+app.post("/addtodo", async (req, res) => {
+  try {
+    const text = req.body.text;
+    const isCompleted = req.body.isCompleted;
+    let response = await fs.promises.readFile("src/db.json", "utf8");
+    let todos = await JSON.parse(response);
+    const todo = {
+      task: text,
+      isCompleted: isCompleted,
+      id: Date.now(),
+    };
+    todos.push(todo);
+    await fs.promises.writeFile("src/db.json", JSON.stringify(todos));
+    // res.status(201).send("To-do item added successfully");
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-app.listen(port,()=>{
-    console.log(`listening to port no. ${port} `);
-})
+app.listen(port, () => {
+  console.log(`listening to port no. ${port} `);
+});
